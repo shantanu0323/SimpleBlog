0b9c8728d05565f49cc4e0abba13ca556710c544
Shantanu Pramanik <srs.shaan@gmail.com>
2017-06-04 22:47:40 +0530

    Initial commit

---
+++ app/src/main/java/shantanu/simpleblog/MainActivity.java
@@ -0,0 +1,122 @@
+package shantanu.simpleblog;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton bAddPost;
+    private RecyclerView blogList;
+    private DatabaseReference databaseReference = null;
+    private ProgressDialog progressDialog = null;
+    private boolean flag = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Blogs");
+
+        blogList.setHasFixedSize(true);
+        blogList.setLayoutManager(new LinearLayoutManager(this));
+
+        bAddPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), NewPost.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        progressDialog.show();
+        FirebaseRecyclerAdapter<Blog, BlogViewHolder> adapter = new FirebaseRecyclerAdapter<Blog, BlogViewHolder>(
+                Blog.class,
+                R.layout.blog_row,
+                BlogViewHolder.class,
+                databaseReference
+        ) {
+            @Override
+            protected void populateViewHolder(BlogViewHolder viewHolder, Blog model, int position) {
+                viewHolder.setTitle(model.getTitle());
+                viewHolder.setDesc(model.getDesc());
+                viewHolder.setImage(getApplicationContext(), model.getImage());
+                if (flag) {
+                    progressDialog.dismiss();
+                    flag = false;
+                }
+            }
+        };
+
+        blogList.setAdapter(adapter);
+    }
+
+    public static class BlogViewHolder extends RecyclerView.ViewHolder {
+
+        View view;
+        public BlogViewHolder(View itemView) {
+            super(itemView);
+            view = itemView;
+        }
+
+        public void setTitle(String title) {
+            TextView tvTitle = (TextView) view.findViewById(R.id.tvTitle);
+            tvTitle.setText(title);
+        }
+
+        public void setDesc(String desc) {
+            TextView tvDesc = (TextView) view.findViewById(R.id.tvDesc);
+            tvDesc.setText(desc);
+        }
+
+        public void setImage(Context context, String imageUrl) {
+            ImageView image = (ImageView) view.findViewById(R.id.image);
+            Picasso.with(context).load(imageUrl).into(image);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.refresh) {
+            // Refresh the News Feeds
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void init() {
+        bAddPost = (FloatingActionButton) findViewById(R.id.bAddPost);
+        blogList = (RecyclerView) findViewById(R.id.blogList);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+    }
+}
---
+++ app/src/main/java/shantanu/simpleblog/NewPost.java
@@ -0,0 +1,119 @@
+package shantanu.simpleblog;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class NewPost extends AppCompatActivity {
+
+    private ImageButton bAddImage;
+    private TextView tvImageLabel;
+    private EditText etTitle;
+    private EditText etDesc;
+    private Button bSubmit;
+
+    private static final int GALLERY_REQUEST = 1;
+    private Uri uri = null;
+    private StorageReference storageReference = null;
+    private DatabaseReference databaseReference = null;
+    private ProgressDialog progressDialog = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_post);
+
+        init();
+
+        progressDialog.setMessage("Posting Blog...");
+        storageReference = FirebaseStorage.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Blogs");
+
+        bAddImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, GALLERY_REQUEST);
+
+            }
+        });
+
+        bSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPosting();
+            }
+        });
+    }
+
+    private void startPosting() {
+        progressDialog.show();
+        final String titleValue = etTitle.getText().toString();
+        final String descValue = etDesc.getText().toString();
+        if (!TextUtils.isEmpty(titleValue) && !TextUtils.isEmpty(descValue) && uri != null) {
+            StorageReference filePath = storageReference.child("BlogImages").child(uri
+                    .getLastPathSegment());
+            filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(NewPost.this, "Blog is posted Successfully!!!", Toast
+                            .LENGTH_SHORT).show();
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    DatabaseReference newPost = databaseReference.push();
+                    newPost.child("title").setValue(titleValue);
+                    newPost.child("desc").setValue(descValue);
+                    newPost.child("image").setValue(downloadUrl.toString());
+
+                    progressDialog.dismiss();
+                    startActivity(new Intent(getBaseContext(), MainActivity.class));
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(NewPost.this, "Failed to Post Blog...", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
+            });
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
+            uri = data.getData();
+            bAddImage.setImageURI(uri);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void init() {
+        bAddImage = (ImageButton) findViewById(R.id.bAddImage);
+        tvImageLabel = (TextView) findViewById(R.id.tvImageLabel);
+        etTitle = (EditText) findViewById(R.id.etTitle);
+        etDesc = (EditText) findViewById(R.id.etDesc);
+        bSubmit = (Button) findViewById(R.id.bSubmit);
+        uri = null;
+        progressDialog = new ProgressDialog(this);
+    }
+}
